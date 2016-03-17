d7f486f01efd9e709e2e1898f3c7c5303f27d5d4
Serge Urakhchin <urakhchin@gmail.com>
2016-03-17 18:17:47 -0500

    Example JS  dynamic app

---
+++ JSProjectEx.js
@@ -0,0 +1,7 @@
+var top_position = 25, left_position = 25; // set up variables
+var width = 300, height = 300;
+var color_list = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+while (width > 50) {
+    //all the following code goes here
+}
+
